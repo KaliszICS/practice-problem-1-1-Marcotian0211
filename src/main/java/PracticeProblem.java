112b01d7587b7146aa3860f79005f9639da80e3f
Marcotian0211 <350327743@tdsb.ca>
2026-02-28 14:38:47 +0000

    update note

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,18 +5,22 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
+		system.out.PrintLn{"hello,world"};
 		//Write question 1 code here
 	}
 
 	public static void q2() {
+		system.out.printLn{"Hello there big falle"};
 		//Write question 2 code here
 	}
 
 	public static void q3() {
+		system.out.prinLn{"4"};
 		//Write question 3 code here
 	}
 
 	public static void q4() {
+		system.out.prinLn{"5.3"};
 		//Write question 4 code here
 	}
 
